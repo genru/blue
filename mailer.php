7115dd9fcb9d87a606dc4dca9387fba2f5dd8841
chris <genjuroa@ymail.com>
2013-11-26 23:43:59 +0800

    update contact recevie email address

--- mailer.php
+++ mailer.php
@@ -1,34 +1,34 @@
-<?PHP 
-$to = "agurghis@yahoo.com"; 
+<?PHP
+$to = "hshinina@163.com";
 $subject = "Messsage from website";
 $headers = "Name: Form Mailer";
 $forward = 1;
 $location = "thankyou.html";
 
-$date = date ("l, F jS, Y"); 
-$time = date ("h:i A"); 
+$date = date ("l, F jS, Y");
+$time = date ("h:i A");
 
 
 
-$msg = "Message sent from website on date  $date, hour: $time.\n\n\n\n"; 
+$msg = "Message sent from website on date  $date, hour: $time.\n\n\n\n";
 
 if ($_SERVER['REQUEST_METHOD'] == "POST") {
-	foreach ($_POST as $key => $value) { 
-		$msg .= ucfirst ($key) ." : ". $value . "\n\n"; 
+	foreach ($_POST as $key => $value) {
+		$msg .= ucfirst ($key) ." : ". $value . "\n\n";
 	}
 }
 else {
-	foreach ($_GET as $key => $value) { 
-		$msg .= ucfirst ($key) ." : ". $value . "\n\n"; 
+	foreach ($_GET as $key => $value) {
+		$msg .= ucfirst ($key) ." : ". $value . "\n\n";
 	}
 }
 
-mail($to, $subject, $msg); 
-if ($forward == 1) { 
-    header ("Location:$location"); 
-} 
-else { 
-    echo "Thank you for your message. We will contact you as soon as possible."; 
-} 
+mail($to, $subject, $msg);
+if ($forward == 1) {
+    header ("Location:$location");
+}
+else {
+    echo "Thank you for your message. We will contact you as soon as possible.";
+}
 
 ?>
